123bdd616a6c08a0ac4ff8e8b0b233b8c3128b8b
simonguenther <simonpublic@fam-guenther.com>
2017-12-06 00:38:39 +0100

    Analyze Block

--- analyzeBlock.py
+++ analyzeBlock.py
@@ -2,6 +2,8 @@
 
 import time; 
 from helper_log import prettify,save_dictionary_to_json
+from analyzeReceipt import get_logs_from_receipt
+from rpc_connector import get_transaction_receipt
 
 def getTransactionsFromBlock(block):
     txs = []
@@ -19,8 +21,20 @@ def getFilteredTransactionsFromBlock(block, filter_list):
         return txs
 
     for tx in block[u'transactions']:
+
         if tx[u'to'] is None: #skip contract creation txs and stuff
             continue
+
+        """    
+        if tx[u'input'] == '0x': # check logs on payable method for interal calls
+            receipt = get_transaction_receipt(tx["hash"])
+            logs = get_logs_from_receipt(receipt)
+            for i in range(0, len(logs)):
+                if logs[i]['address'].upper() in filter_list.keys():
+                    tx["ico_id"] = filter_list[logs[i]['address'].upper()]
+                    txs.append(tx)
+                    break
+        """
 
         if tx[u'to'].upper() in filter_list.keys():
             tx["ico_id"] = filter_list[tx['to'].upper()]

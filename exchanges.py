8770369cfc03d79b7b0dc84f256749fecd701efd
simonguenther <simonpublic@fam-guenther.com>
2017-12-30 10:48:58 +0100

    added huobi

---
+++ exchanges.py
@@ -0,0 +1,58 @@
+poloniex = []
+kraken = []
+bittrex = []
+bitfinex = []
+binance = []
+liqui = []
+etherdelta = []
+yunbi = []
+huobi = []
+"""
+--"0x05ee546c1a62f90d7acbffd6d846c9c54c7cf94c"
+--"0x0b078896a3d9166da5c37ae52a5809aca48630d4"
+--"0xfe9e8709d3215310075d67e3ed32a380ccf451c8" -- binance cold wallet?
+--0x2b5634c42055806a59e9107ed44d43c426e58258
+--0x05ee546c1a62f90d7acbffd6d846c9c54c7cf94c
+"""
+
+
+liqui[0] = "0x8271b2e8cbe29396e9563229030c89679b9470db"
+liqui[1] = "0x5e575279bf9f4acf0a130c186861454247394c06"
+
+poloniex[0] = "0xb794f5ea0ba39494ce839613fffba74279579268" #polo coldwallet
+poloniex[1] = "0x32be343b94f860124dc4fee278fdcbd38c102d88"
+
+kraken[0] = "0xe853c56864a2ebe4576a807d26fdc4a0ada51919"
+kraken[1] = "0x267be1c1d684f78cb4f6a176c4911b741e4ffdc0"
+kraken[2] = "0x0a869d79a7052c7f1b55a8ebabbea3420f0d1e13"
+kraken[3] = "0x2910543af39aba0cd09dbb2d50200b3e800a63d2"
+
+bittrex[0] = "0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98"
+
+bitfinex[0] = "0x4fdd5eb2fb260149a3903859043e962ab89d8ed4"
+bitfinex[1] = "0x1151314c646ce4e0efd76d1af4760ae66a9fe30f"
+bitfinex[2] = "0x7727e5113d1d161373623e5f49fd568b4f543a9e"
+bitfinex[3] = "0xcafb10ee663f465f9d10588ac44ed20ed608c11e"
+bitfinex[4] = "0x7180EB39A6264938FDB3EfFD7341C4727c382153"
+
+binance[0] = "0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be"
+
+etherdelta[0] = "0x4aEa7cf559F67ceDCAD07E12aE6bc00F07E8cf65"
+etherdelta[1] = "0x8d12a197cb00d4747a1fe03395095ce2a5cc6819"
+
+yunbi[0] = "0xd94c9ff168dc6aebf9b6cc86deff54f3fb0afc33"
+yunbi[1] = "0x42da8a05cb7ed9a43572b5ba1b8f82a0a6e263dc"
+yunbi[2] = "0x700f6912e5753e91ea3fae877a2374a2db1245d7"
+
+huobi[0] = "0x46705dfff24256421A05D056c29E81Bdc09723B8"
+
+# eigene: 
+"""
+            wallet => deposit address :     https://etherscan.io/tx/0x82829f14c3b8a85df674bb495b7aeb673249677b38cb86da969750271d143f66
+1.5h later  deposit addr => hitbtc contract https://etherscan.io/tx/0x3a17d21ff7b675842c6722e47b0b7b862e2a6fb7c4525c8b1938c1da272af902
+
+out
+            hitbtc => vib wallet:           https://etherscan.io/tx/0x39368f96de84188c6f1c44e160be1958829d9c1c00b4728538a55190891a413b
+1h later    vib wallet => etherdelta        https://etherscan.io/tx/0x35ed54d963cc1e910f169e2599013256112cd1d32a86f2714be180c279a840da        
+
+"""
